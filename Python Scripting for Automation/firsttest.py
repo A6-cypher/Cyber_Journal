fde2a26ca195a2f362dbd9c07235e3ba4ebd917f
A6-cypher <arusseean11@gmail.com>
2024-02-12 21:22:59 +0400

    Added More on Sql injections and started project on python

---
+++ Python Scripting for Automation/firsttest.py
@@ -0,0 +1,54 @@
+print("hello world")
+
+x=10
+y=120
+
+print(x+y)
+
+if y > x:
+    print("{y} is greater than {x}")
+    print("dsadas")
+    
+print("We are ok now")
+
+#Now we are writing python comments
+#So Now we know it is not running these lines
+#Comments are a guide to understand the codes
+
+'''
+are we a comment?
+'''
+
+print("YES!!")
+
+#Now we are going to print using special characters
+
+print("Testing Special Characters")
+print("Testing it out")
+
+print("Hello World\nWe are now on the next line\b\b\b\bworld")
+print("Hii \a")
+'''
+Special Chars
+\n new line
+\b backspace
+\t tab space
+\ Escape symbol(Example print("this is a \"New" line"\))
+\a alert sound
+'''
+
+#Running python scripts on terminal
+
+'''
+So, we can add the path to the first line
+
+Example
+
+#! /usr/local/bin/python3 #Mentioning the python compiler
+
+Then give execution permission the script file
+
+chmod +x hello.py
+
+write the path of the script and it will run!!
+'''
